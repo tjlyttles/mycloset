e7a2387da23a6fb8032d6ce58e6c01a84ea4add2
tjlyttles <tjlyttles84@gmail.com>
2019-06-15 18:29:38 -0700

    merge

--- models/imageurl.js
+++ models/imageurl.js
@@ -2,5 +2,12 @@ module.exports = function(sequelize, DataTypes) {
   var Url = sequelize.define("Url", {
     imgUrl: DataTypes.STRING
   });
+  Url.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Url.hasMany(models.Shirt, {
+      onDelete: "cascade"
+    });
+  };
   return Url;
 };
